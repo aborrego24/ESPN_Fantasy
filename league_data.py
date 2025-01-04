04d1a8e1803c7b1f149b782e436159881d19ac65
Alejandro Borrego <borregoa@umich.edu>
2025-01-04 15:15:13 -0500

    initial commit + links

--- league_data.py
+++ league_data.py
@@ -6,3 +6,8 @@ league = League(league_id=123564885, year=2024)
 # Print team names
 for team in league.teams:
     print(team.team_name)
+
+
+# Link to API:
+#   https://github.com/cwendt94/espn-api/wiki
+#   https://github.com/cwendt94/espn-api?tab=readme-ov-file
